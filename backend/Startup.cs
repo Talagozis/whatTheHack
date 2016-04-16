70987a20eb2c9c76e84d7eae4e6e22323aed6a6e
Talagozis <talagozis8@gmail.com>
2016-04-16 13:31:53 +0300

    FirstOrDefault

---
+++ backend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(whatTheHack.Startup))]
+namespace whatTheHack
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
